01fd661e92bef64de2db5e9d8637d360b639c081
Love Lyckaro <love.lyckaro@hotmail.se>
2023-12-01 07:31:03 +0100

    Solve day 1

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,18 +1,47 @@
 module Main where
 
+import Data.Char
+import Data.Map qualified as M
+import Replace.Megaparsec
 import SantaLib
 import SantaLib.Parsing
 
-pInp :: Parser [[Int]]
-pInp = lineSepNumbers `sepBy` eol
+pDigit :: Parser Integer
+pDigit = do
+  d <- digitChar
+  return (read [d])
 
-part1 = id
+digits :: [(String, Integer)]
+digits = zip ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] [1 ..]
 
-part2 = id
+pSpelledOutDigit :: Parser Integer
+pSpelledOutDigit = choice $ map (\(spelling, digit) -> digit <$ try (string spelling)) digits
+
+pSpelledOutBackwardsDigit :: Parser Integer
+pSpelledOutBackwardsDigit = choice $ map (\(spelling, digit) -> digit <$ try (string (reverse spelling))) digits
+
+firstlastdigit :: String -> Integer
+firstlastdigit str = read [head digits, last digits]
+  where
+    digits = filter isDigit str
+
+part1 :: String -> Integer
+part1 = sum . map firstlastdigit . lines
+
+firstlastdigit2 :: String -> Integer
+firstlastdigit2 line = firstdigit * 10 + lastdigit
+  where
+    pFirst = pDigit <|> pSpelledOutDigit
+    pLast = pDigit <|> pSpelledOutBackwardsDigit
+    Just (_prefix, firstdigit, rest) = breakCap pFirst line
+    (_suffix, lastdigit, _middle) = case breakCap pLast (reverse rest) of
+      Nothing -> (undefined, firstdigit, undefined)
+      Just x -> x
+
+part2 = sum . map firstlastdigit2 . lines
 
 main :: IO ()
 main = do
-  inp <- getInput 1 >>= parseIO pInp "day1.input"
-  -- example <- getExample 1 >>= parseIO pInp "day1-example.input"
+  inp <- getInput 1
   putAnswer 1 Part1 (part1 inp)
   putAnswer 1 Part2 (part2 inp)
