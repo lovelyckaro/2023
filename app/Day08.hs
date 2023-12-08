0891b4a84689437be85684024f04712a12e2bb18
Love Lyckaro <love.lyckaro@tietoevry.com>
2023-12-08 06:50:37 +0100

    Solve day 8

--- app/Day08.hs
+++ app/Day08.hs
@@ -1,14 +1,50 @@
 module Main where
 
+import Data.List
+import Data.Map (Map, (!))
+import Data.Map qualified as M
 import SantaLib
 import SantaLib.Parsing
 
-pInp :: Parser ()
-pInp = return ()
+type Node = String
 
-part1 = id
+type Graph = Map Node (Node, Node)
 
-part2 = id
+data Direction = L | R
+  deriving (Show)
+
+pInp :: Parser ([Direction], Graph)
+pInp = do
+  directions <- some (L <$ char 'L' <|> R <$ char 'R')
+  eol >> eol
+  graphElems <- some $ lexemeLn $ do
+    node <- lexeme (some upperChar)
+    symbol "="
+    symbol "("
+    left <- lexeme (some upperChar)
+    symbol ","
+    right <- lexeme (some upperChar)
+    symbol ")"
+    return (node, (left, right))
+  return (directions, M.fromList graphElems)
+
+followDirections :: Graph -> (Node -> Bool) -> [Direction] -> Node -> [Node]
+followDirections graph goal (dir : dirs) curr
+  | goal curr = []
+  | otherwise =
+      let (left, right) = graph ! curr
+       in case dir of
+            L -> left : followDirections graph goal dirs left
+            R -> right : followDirections graph goal dirs right
+
+part1 :: ([Direction], Graph) -> Int
+part1 (instrs, graph) = length $ followDirections graph (== "ZZZ") (cycle instrs) "AAA"
+
+part2 :: ([Direction], Graph) -> Int
+part2 (instrs, graph) = foldr lcm 1 times
+  where
+    starts = [node | node <- M.keys graph, "A" `isSuffixOf` node]
+    times = map (length . followDirections graph ("Z" `isSuffixOf`) (cycle instrs)) starts
 
 main :: IO ()
 main = do
