c96d175eb572f133423418cf4df42e47dbab0551
Love Lyckaro <love.lyckaro@tietoevry.com>
2023-12-09 06:23:08 +0100

    Add solution for day 9

--- app/Day09.hs
+++ app/Day09.hs
@@ -3,12 +3,34 @@ module Main where
 import SantaLib
 import SantaLib.Parsing
 
-pInp :: Parser ()
-pInp = return ()
+pInp :: Parser [[Int]]
+pInp = some $ lexemeLn $ some $ lexeme $ signed decimal
 
-part1 = id
+diffs :: [Int] -> [Int]
+diffs xs = zipWith (-) (tail xs) xs
 
-part2 = id
+allDiffs :: [Int] -> [[Int]]
+allDiffs xs
+  | all (== 0) (diffs xs) = [diffs xs]
+  | otherwise =
+      let ds = diffs xs
+       in ds : allDiffs ds
+
+extrapolateNext :: [Int] -> Int
+extrapolateNext xs = sum (map last (xs : allDiffs xs))
+
+subHalf :: [Int] -> Int
+subHalf [] = 0
+subHalf [x] = x
+subHalf (x : y : xs) = x - y + subHalf xs
+
+extrapolatePrev :: [Int] -> Int
+extrapolatePrev xs = subHalf (map head (xs : allDiffs xs))
+
+part1 :: [[Int]] -> Int
+part1 = sum . map extrapolateNext
+
+part2 = sum . map extrapolatePrev
 
 main :: IO ()
 main = do
